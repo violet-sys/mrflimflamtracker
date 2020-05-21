d94b3d9724a16b5a6a96028988c3db64f57b1f31
violet-sys <64088110+violet-sys@users.noreply.github.com>
2020-05-21 17:29:02 -0500

    add pt files

---
+++ pt_stats.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+gc = 26
+fc = 200
+ac = 34
+Tk = obj
+obj=Tk()
+obj.title("Mr. FlimFlam Rastreador")
+obj.geometry("300x300")
+Text(obj) = wintext
+wintext = Text(obj)
+wintext.insert("Grupo contagem; " + gc)
+wintext.insert("Amigo contagem; " + fc)
+wintext.insert("Alt contagem; " + ac + "URL; https://bit.ly/2ymeeXb")
+wintext.insert("Hackeado alt URL; https://bit.ly/3cSJWKy")
